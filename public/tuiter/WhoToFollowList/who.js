0239d253e3e7138ce0f25c70250d2452068877c0
Catalina Gutierrez <catalinagutierrez@Catalinas-MacBook-Pro.local>
2022-02-22 16:37:09 -0500

    a5 JavaScript sp22

--- public/tuiter/WhoToFollowList/who.js
+++ public/tuiter/WhoToFollowList/who.js
@@ -1,15 +1,15 @@
 export default [
-  { avatarIcon: "../../images/java.png", userName: "Java", handle: "Java" },
+  { avatarIcon: "../images/java.png", userName: "Java", handle: "Java" },
   {
-    avatarIcon: "../../images/rs.png",
+    avatarIcon: "../images/rs.png",
     userName: "Relativity Space",
     handle: "relativityspace",
   },
   {
-    avatarIcon: "../../images/virgin.png",
+    avatarIcon: "../images/virgin.png",
     userName: "Virgin Galactic",
     handle: "virgingalactic",
   },
-  { avatarIcon: "../../images/nasa.jpg", userName: "NASA", handle: "NASA" },
-  { avatarIcon: "../../images/tesla.png", userName: "Tesla", handle: "Tesla" },
+  { avatarIcon: "../images/nasa.jpg", userName: "NASA", handle: "NASA" },
+  { avatarIcon: "../images/tesla.png", userName: "Tesla", handle: "Tesla" },
 ];
