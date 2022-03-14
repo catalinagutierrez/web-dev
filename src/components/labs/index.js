6caad3f4a8f016691bf2d940854b975c73d9c0d9
Catalina <cgutierrezm95@gmail.com>
2022-03-14 15:54:43 -0400

    a6 React.js sp22

--- src/components/labs/index.js
+++ src/components/labs/index.js
@@ -10,7 +10,7 @@ const Labs = () => {
   return (
     <>
       <h1>Labs</h1>
-      <Link to="/hello">Hello</Link> |<Link to="/tuiter">Tuiter</Link>
+      <Link to="/hello">Hello</Link> |<Link to="/tuiter/home">Tuiter</Link>
       <ConditionalOutput />
       <Styles />
       <Classes />
